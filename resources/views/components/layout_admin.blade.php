8621cca578ea319ec9dc88819871a1adea0c006e
Nilfgard13 <fachrizalfazza@gmail.com>
2024-08-19 22:40:13 +0700

    blade templating

--- resources/views/components/layout_admin.blade.php
+++ resources/views/components/layout_admin.blade.php
@@ -46,8 +46,6 @@
                         <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span>
                             <span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse in">
-                            {{-- <li class="active"><a href="/admin">Rotator Management</a></li>
-                            <li><a href="/profile">Admin Profile</a></li> --}}
                             <x-navbar href="/admin">Rotator Management</x-navbar>
                             <x-navbar href="/profile">Admin Profile</x-navbar>
                         </ul>
@@ -119,62 +117,8 @@
                 </nav>
             </div>
             <div class="wrapper wrapper-content">
-                <div class="row">
-                    <div class="col-lg-3">
-                        <div class="ibox float-e-margins">
-                            <div class="ibox-title">
-                                <span class="label label-success pull-right">Monthly</span>
-                                <h5>Income</h5>
-                            </div>
-                            <div class="ibox-content">
-                                <h1 class="no-margins">40 886,200</h1>
-                                <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
-                                <small>Total income</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="ibox float-e-margins">
-                            <div class="ibox-title">
-                                <span class="label label-info pull-right">Annual</span>
-                                <h5>Orders</h5>
-                            </div>
-                            <div class="ibox-content">
-                                <h1 class="no-margins">275,800</h1>
-                                <div class="stat-percent font-bold text-info">20% <i class="fa fa-level-up"></i></div>
-                                <small>New orders</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="ibox float-e-margins">
-                            <div class="ibox-title">
-                                <span class="label label-primary pull-right">Today</span>
-                                <h5>visits</h5>
-                            </div>
-                            <div class="ibox-content">
-                                <h1 class="no-margins">106,120</h1>
-                                <div class="stat-percent font-bold text-navy">44% <i class="fa fa-level-up"></i></div>
-                                <small>New visits</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="ibox float-e-margins">
-                            <div class="ibox-title">
-                                <span class="label label-danger pull-right">Low value</span>
-                                <h5>User activity</h5>
-                            </div>
-                            <div class="ibox-content">
-                                <h1 class="no-margins">80,600</h1>
-                                <div class="stat-percent font-bold text-danger">38% <i class="fa fa-level-down"></i>
-                                </div>
-                                <small>In first month</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+                {{-- body content --}}
+                {{ $slot }}
             </div>
             <div class="footer">
                 <div class="pull-right">
