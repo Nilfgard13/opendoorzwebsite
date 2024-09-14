33162cf4e30f5e6666ce405c1052fa12ac656fce
Nilfgard13 <fachrizalfazza@gmail.com>
2024-09-14 08:22:57 +0700

    update navlink 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -54,14 +54,7 @@
 
                         <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
 
-                        {{-- <a href="/forget">
-                            <small>Forgot password?</small>
-                        </a> --}}
-
-                        {{-- <p class="text-muted text-center">
-                            <small>Do not have an account?</small>
-                        </p>
-                        <a class="btn btn-sm btn-white btn-block" href="{{ route('register') }}">Create an account</a> --}}
+                        
 
                     </form>
 
